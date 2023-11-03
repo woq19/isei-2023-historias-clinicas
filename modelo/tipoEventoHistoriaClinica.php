49e2bfd1b7557bea39adc9b4fff80eeec1fa95dd
emanuel <bianchimauricio14@gmail.com>
2023-11-03 20:14:43 -0300

    merge

--- modelo/tipoEventoHistoriaClinica.php
+++ modelo/tipoEventoHistoriaClinica.php
@@ -1,27 +1,17 @@
-
 <?php
-class TipoEventoHistoriaClinica {
+class TipoEventoHistoriaClinica{ 
 
-    
     public $Id;
     public $Descripcion;
-    public $Eliminado;
 
-    public static function Buscar($id)
+    public $Eliminado;
+public function Eliminar()
     {
-        $con  = Database::getInstance();
-        $sql = "select * from tipoeventohistoriaclinica where Id = :p1";
-        $queryClaseAReemplazar = $con->db->prepare($sql);
-        $params = array("p1" => $id);
-        $queryClaseAReemplazar->execute($params);
-        $queryClaseAReemplazar->setFetchMode(PDO::FETCH_CLASS, 'TipoEventoHistoriaClinica');
-        foreach ($queryClaseAReemplazar as $m) {
-            return $m;
-        }
+        $con = Database::getInstance();
+        $sql = "UPDATE tipoeventohistoriaclinica SET Eliminado=1 WHERE Id = :p1";
+        $claseAReemplazar = $con->db->prepare($sql);
+        $params = array("p1" => $this->Id);
+        $claseAReemplazar->execute($params);
     }
 
-
-
 }
-
-
