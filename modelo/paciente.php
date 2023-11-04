4c2c51bd572af55ccd16e054ec297e5f70bf3c2b
0tsuguA <augusstotl@gmail.com>
2023-11-03 22:29:16 -0300

    Fix-consultar-paciente

--- modelo/paciente.php
+++ modelo/paciente.php
@@ -10,6 +10,19 @@ class Paciente{
     public $GrupoSanguineo;
     public $ObraSocialId;
 
+    public static function Buscar($id)
+    {
+        $con  = Database::getInstance();
+        $sql = "select * from paciente where Id = :p1";
+        $queryClaseAReemplazar = $con->db->prepare($sql);
+        $params = array("p1" => $id);
+        $queryClaseAReemplazar->execute($params);
+        $queryClaseAReemplazar->setFetchMode(PDO::FETCH_CLASS, 'Paciente');
+        foreach ($queryClaseAReemplazar as $m) {
+            return $m;
+        }
+    }
+
     public static function BuscarTodos()
     {
         $con  = Database::getInstance();
