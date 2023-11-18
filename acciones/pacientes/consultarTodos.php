faea750f0603f898ab1a20b124bbe48577ff6c10
Flor Farruggia <flor.farru@gmail.com>
2023-11-17 21:27:49 -0300

    fix header consultar pacientes

--- acciones/pacientes/consultarTodos.php
+++ acciones/pacientes/consultarTodos.php
@@ -3,6 +3,7 @@
 require_once '../pacientes/responses/consultarTodosResponse.php';
 require_once '../../modelo/paciente.php';
 require_once '../../configuracion/database.php';
+header('Content-Type: application/json');
 
 $resp= new ConsultarTodosResponse();
 
